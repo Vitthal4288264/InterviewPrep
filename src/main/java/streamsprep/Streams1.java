fd7eb4bfd6fe75870032ffc754ee7db6afc3970f
SrishaVitthal <>
2023-11-07 18:10:46 +0100

    Refactoring of packages.

--- src/main/java/streamsprep/Streams1.java
+++ src/main/java/streamsprep/Streams1.java
@@ -1,13 +1,17 @@
 package streamsprep;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Streams1 {
 
-
+static String pattern = "==============================================================================================";
     public static void main(String[] args) {
         List<Emp> empList = buildEmpData();
         //1.
@@ -20,21 +24,130 @@ public class Streams1 {
 
         //3
         //Find Average age of male and female employees
+        calculateAverageAgeOfEmp(empList);
+
+        //4.
+        //Get the Names of Employees who joined after 2015
+        getEmpsByYearOfJoin(empList);
+
+        //5.
+        //Count the employees in the department
+        countEmpsByDepartment(empList);
+
+        //6.
+        //Find out the average salary of each department
+        averageSalaryByDepartment(empList);
+
+        //7.
+        //Find out the oldest employee, his/her age and department?
+        getOldestEmployee(empList);
+
+        //8.
+        //Find average salary and total salary of the organization.
+        averageSalaryOfOrg(empList);
+
+        salaryDetailsForDepartment(empList);
+    }
+
+    private static void salaryDetailsForDepartment(List<Emp> empList) {
+        System.out.println(pattern);
+        System.out.println("Average salary details for each department =>");
+        Map<String,DoubleSummaryStatistics> collect =
+                empList.stream()
+                        .collect(Collectors.groupingBy(Emp::getDepartment,
+                                Collectors.summarizingDouble(Emp::getSalary)));
+        Set<Map.Entry<String, DoubleSummaryStatistics>> entrySet = collect.entrySet();
+
+        for(Map.Entry entry: entrySet){
+            System.out.println("Department: "+ entry.getKey()+"\n");
+            var value = (DoubleSummaryStatistics) entry.getValue();
+            System.out.println("Average salary => "+value.getAverage());
+            System.out.println("Total salary =>"+value.getSum()+"\n");
+
+        }
+        System.out.println(pattern);
+    }
+
+    private static void averageSalaryOfOrg(List<Emp> empList) {
+        System.out.println(pattern);
+        System.out.println("Average Salary details of organisation =>");
+        DoubleSummaryStatistics empSalary = empList.stream()
+                .collect(Collectors.summarizingDouble(Emp::getSalary));
+        System.out.println("Average Salary => "+empSalary.getAverage());
+        System.out.println("Total Salary =>"+empSalary.getSum());
+        System.out.println(pattern);
+    }
+
+    private static void getOldestEmployee(List<Emp> empList) {
+        System.out.println(pattern);
+        //max() takes comparator as we are comparing age which of type int, so we are using comparingInt()
+        Optional<Emp> emp = empList.stream()
+                .max(Comparator.comparingInt(Emp::getAge));
+        Emp emp1 = emp.orElseThrow(() -> new RuntimeException("No value found!!"));
+        System.out.println(" Name : "+emp1.getName()+"\n Age : "+ emp1.getAge()+"\n Department : "+emp1.getDepartment());
+        System.out.println(pattern);
+    }
+
+    private static void averageSalaryByDepartment(List<Emp> empList) {
+        System.out.println(pattern);
+        System.out.println("Print the average salary by Department");
+        Map<String, Double> map =
+        empList.stream().collect(Collectors.groupingBy(
+                Emp::getDepartment, Collectors.averagingDouble(Emp::getSalary)
+        ));
+        Set<Map.Entry<String, Double>> entrySet = map.entrySet();
+        for(Map.Entry entry: entrySet){
+            System.out.println(entry.getKey()+ ": "+entry.getValue());
+        }
+        System.out.println(pattern);
+    }
+
+    private static void countEmpsByDepartment(List<Emp> empList) {
+        System.out.println(pattern);
+        System.out.println("Count the employees in the department!!\n");
+        Map<String, Long> map = empList.stream()
+                .collect(Collectors.groupingBy(
+                        Emp::getDepartment, Collectors.counting()
+                ));
+        Set<Map.Entry<String, Long>> entrySet = map.entrySet();
+        //entrySet.stream().map(entry -> entry.getKey() + ": " + entry.getValue()).forEach(System.out::println);
+        for(Map.Entry entry : entrySet){
+            System.out.println(entry.getKey() + ": "+entry.getValue());
+        }
+        System.out.println(pattern);
+    }
+
+    private static void getEmpsByYearOfJoin(List<Emp> empList) {
+        System.out.println(pattern);
+        System.out.println("Employees who joined after 2015 -> ");
+        empList.stream().filter(n -> n.yearOfJoining > 2015)
+                .map(Emp::getName)
+                .sorted()
+                .forEach(System.out :: println);
+        System.out.println(pattern);
+    }
+
+    private static void calculateAverageAgeOfEmp(List<Emp> empList) {
+        System.out.println(pattern);
         Map<String, Double> averageAge = empList.stream().collect(Collectors.groupingBy(Emp::getGender,
-                                                       Collectors.averagingInt(
+                                                                                        Collectors.averagingInt(
                                                                Emp::getAge)));
         System.out.println("Average age of employees by gender =>"+averageAge);
+        System.out.println(pattern);
     }
 
     private static void findAllDepartments(List<Emp> empList) {
         //first map only department from Emp
         //then perform distinct on map data
         //finally print the values
+        System.out.println(pattern);
         System.out.println("List of departments :: ");
         empList.stream().map(Emp::getDepartment).distinct().forEach(System.out::println);
+        System.out.println(pattern);
     }
 
     private static void findEmpCount(List<Emp> empList) {
+        System.out.println(pattern);
         //this is straight forward approach
         long maleEmpCount = empList.stream().filter(n -> n.gender.equals("Male")).count();
         System.out.println("number of employees =>"+ maleEmpCount);
@@ -49,6 +162,7 @@ public class Streams1 {
                         Emp::getGender, Collectors.counting()
                 ));
         System.out.println("Employee count :: "+empCount);
+        System.out.println(pattern);
     }
 
     private static List<Emp> buildEmpData() {
